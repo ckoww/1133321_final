8d27ab5c435256cd6378ef06b2732d337ae77001
ckoww <hanna22095@gmail.com>
2026-06-16 17:22:45 +0800

    加入專案檔案。

---
+++ 1133321_final/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _1133321_final
+{
+    partial class hotel
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpRooms = new System.Windows.Forms.TableLayoutPanel();
+            this.grpEdit = new System.Windows.Forms.GroupBox();
+            this.lblRoomNumTitle = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblStatusTitle = new System.Windows.Forms.Label();
+            this.lblNotesTitle = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblRoomNum = new System.Windows.Forms.Label();
+            this.dtpCheckOut = new System.Windows.Forms.DateTimePicker();
+            this.lblCheckOut = new System.Windows.Forms.Label();
+            this.lblService = new System.Windows.Forms.Label();
+            this.cmbService = new System.Windows.Forms.ComboBox();
+            this.grpEdit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpRooms
+            // 
+            this.tlpRooms.ColumnCount = 3;
+            this.tlpRooms.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.13187F));
+            this.tlpRooms.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.86813F));
+            this.tlpRooms.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 223F));
+            this.tlpRooms.Location = new System.Drawing.Point(12, 12);
+            this.tlpRooms.Name = "tlpRooms";
+            this.tlpRooms.RowCount = 2;
+            this.tlpRooms.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.45631F));
+            this.tlpRooms.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.54369F));
+            this.tlpRooms.Size = new System.Drawing.Size(679, 206);
+            this.tlpRooms.TabIndex = 0;
+            // 
+            // grpEdit
+            // 
+            this.grpEdit.Controls.Add(this.cmbService);
+            this.grpEdit.Controls.Add(this.lblService);
+            this.grpEdit.Controls.Add(this.lblCheckOut);
+            this.grpEdit.Controls.Add(this.dtpCheckOut);
+            this.grpEdit.Controls.Add(this.lblRoomNum);
+            this.grpEdit.Controls.Add(this.btnCancel);
+            this.grpEdit.Controls.Add(this.lblNotesTitle);
+            this.grpEdit.Controls.Add(this.lblStatusTitle);
+            this.grpEdit.Controls.Add(this.btnSave);
+            this.grpEdit.Controls.Add(this.txtNotes);
+            this.grpEdit.Controls.Add(this.cmbStatus);
+            this.grpEdit.Controls.Add(this.lblRoomNumTitle);
+            this.grpEdit.Enabled = false;
+            this.grpEdit.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.grpEdit.Location = new System.Drawing.Point(12, 224);
+            this.grpEdit.Name = "grpEdit";
+            this.grpEdit.Size = new System.Drawing.Size(679, 253);
+            this.grpEdit.TabIndex = 1;
+            this.grpEdit.TabStop = false;
+            // 
+            // lblRoomNumTitle
+            // 
+            this.lblRoomNumTitle.AutoSize = true;
+            this.lblRoomNumTitle.Location = new System.Drawing.Point(42, 19);
+            this.lblRoomNumTitle.Name = "lblRoomNumTitle";
+            this.lblRoomNumTitle.Size = new System.Drawing.Size(98, 13);
+            this.lblRoomNumTitle.TabIndex = 0;
+            this.lblRoomNumTitle.Text = "目前選取房號：";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "入住中",
+            "待清潔",
+            "維修中",
+            "空房"});
+            this.cmbStatus.Location = new System.Drawing.Point(536, 48);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(128, 21);
+            this.cmbStatus.TabIndex = 1;
+            this.cmbStatus.SelectedIndexChanged += new System.EventHandler(this.cmbStatus_SelectedIndexChanged);
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(139, 45);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(288, 202);
+            this.txtNotes.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(499, 224);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblStatusTitle
+            // 
+            this.lblStatusTitle.AutoSize = true;
+            this.lblStatusTitle.Location = new System.Drawing.Point(461, 51);
+            this.lblStatusTitle.Name = "lblStatusTitle";
+            this.lblStatusTitle.Size = new System.Drawing.Size(72, 13);
+            this.lblStatusTitle.TabIndex = 4;
+            this.lblStatusTitle.Text = "房況狀態：";
+            // 
+            // lblNotesTitle
+            // 
+            this.lblNotesTitle.AutoSize = true;
+            this.lblNotesTitle.Location = new System.Drawing.Point(7, 48);
+            this.lblNotesTitle.Name = "lblNotesTitle";
+            this.lblNotesTitle.Size = new System.Drawing.Size(133, 13);
+            this.lblNotesTitle.TabIndex = 5;
+            this.lblNotesTitle.Text = "客房備註 / 交接事項：";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(589, 224);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblRoomNum
+            // 
+            this.lblRoomNum.AutoSize = true;
+            this.lblRoomNum.Location = new System.Drawing.Point(138, 19);
+            this.lblRoomNum.Name = "lblRoomNum";
+            this.lblRoomNum.Size = new System.Drawing.Size(11, 13);
+            this.lblRoomNum.TabIndex = 7;
+            this.lblRoomNum.Text = "-";
+            // 
+            // dtpCheckOut
+            // 
+            this.dtpCheckOut.Location = new System.Drawing.Point(536, 76);
+            this.dtpCheckOut.Name = "dtpCheckOut";
+            this.dtpCheckOut.Size = new System.Drawing.Size(128, 23);
+            this.dtpCheckOut.TabIndex = 2;
+            // 
+            // lblCheckOut
+            // 
+            this.lblCheckOut.AutoSize = true;
+            this.lblCheckOut.Location = new System.Drawing.Point(435, 83);
+            this.lblCheckOut.Name = "lblCheckOut";
+            this.lblCheckOut.Size = new System.Drawing.Size(98, 13);
+            this.lblCheckOut.TabIndex = 8;
+            this.lblCheckOut.Text = "預計退房日期：";
+            // 
+            // lblService
+            // 
+            this.lblService.AutoSize = true;
+            this.lblService.Location = new System.Drawing.Point(435, 112);
+            this.lblService.Name = "lblService";
+            this.lblService.Size = new System.Drawing.Size(98, 13);
+            this.lblService.TabIndex = 9;
+            this.lblService.Text = "客房服務指示：";
+            // 
+            // cmbService
+            // 
+            this.cmbService.FormattingEnabled = true;
+            this.cmbService.Items.AddRange(new object[] {
+            "無指示(正常)",
+            "請勿打擾 (DND)",
+            "請即打掃 (MUR)"});
+            this.cmbService.Location = new System.Drawing.Point(536, 109);
+            this.cmbService.Name = "cmbService";
+            this.cmbService.Size = new System.Drawing.Size(128, 21);
+            this.cmbService.TabIndex = 10;
+            // 
+            // hotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 489);
+            this.Controls.Add(this.grpEdit);
+            this.Controls.Add(this.tlpRooms);
+            this.Name = "hotel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "hotel";
+            this.grpEdit.ResumeLayout(false);
+            this.grpEdit.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpRooms;
+        private System.Windows.Forms.GroupBox grpEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label lblRoomNumTitle;
+        private System.Windows.Forms.Label lblNotesTitle;
+        private System.Windows.Forms.Label lblStatusTitle;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblRoomNum;
+        private System.Windows.Forms.DateTimePicker dtpCheckOut;
+        private System.Windows.Forms.ComboBox cmbService;
+        private System.Windows.Forms.Label lblService;
+        private System.Windows.Forms.Label lblCheckOut;
+    }
+}
+
---
+++ 1133321_final/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+// 🌟 1. 換成全新的 WinForms 專用命名空間
+using System.Data.SQLite;
+
+namespace _1133321_final
+{
+    public partial class hotel : Form
+    {
+        // 🌟 2. 連線字串加上 Version=3，這是 System.Data.SQLite 的標準寫法
+        private string connectionString = "Data Source=hotel_management.db;Version=3;";
+        private string selectedRoomNumber = "";
+
+        public hotel()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(hotel_Load);
+        }
+
+        private void hotel_Load(object sender, EventArgs e)
+        {
+            dtpCheckOut.MinDate = DateTime.Today;
+            
+            InitializeDatabase();
+            LoadRoomButtons();
+        }
+
+        #region 資料庫初始化
+        private void InitializeDatabase()
+        {
+            // 🌟 3. 所有的 SqliteConnection 都改成 SQLiteConnection (L大寫)
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Rooms (
+                        RoomNumber TEXT PRIMARY KEY,
+                        Status TEXT NOT NULL,
+                        CheckOutDate TEXT,
+                        ServiceStatus TEXT,
+                        GuestNotes TEXT
+                    );";
+
+                using (var command = new SQLiteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                string checkEmptyQuery = "SELECT COUNT(*) FROM Rooms;";
+                using (var command = new SQLiteCommand(checkEmptyQuery, connection))
+                {
+                    long count = (long)command.ExecuteScalar();
+                    if (count == 0)
+                    {
+                        string[] initialRooms = { "101", "102", "103", "104", "105", "106" };
+
+                        foreach (var room in initialRooms)
+                        {
+                            string insertQuery = @"
+                                INSERT INTO Rooms (RoomNumber, Status, CheckOutDate, ServiceStatus, GuestNotes) 
+                                VALUES (@RoomNum, '空房', '', '無指示', '');";
+
+                            using (var insertCmd = new SQLiteCommand(insertQuery, connection))
+                            {
+                                insertCmd.Parameters.AddWithValue("@RoomNum", room);
+                                insertCmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region 動態產生左側房間按鈕
+        private void LoadRoomButtons()
+        {
+            tlpRooms.Controls.Clear();
+            tlpRooms.RowStyles.Clear();
+            tlpRooms.RowCount = 0;
+
+            int currentColumnCount = tlpRooms.ColumnCount > 0 ? tlpRooms.ColumnCount : 2;
+            int roomCount = 0;
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string selectQuery = "SELECT RoomNumber, Status, ServiceStatus FROM Rooms ORDER BY RoomNumber;";
+
+                using (var command = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string roomNum = reader["RoomNumber"].ToString();
+                            string status = reader["Status"].ToString();
+                            string service = reader["ServiceStatus"].ToString();
+
+                            Button btnRoom = new Button();
+
+                            string buttonText = $"{roomNum}\n({status})";
+                            if (service == "請勿打擾 (DND)") buttonText += "\n🚫 DND";
+                            else if (service == "請即打掃 (MUR)") buttonText += "\n🧹 需打掃";
+
+                            btnRoom.Text = buttonText;
+                            btnRoom.Dock = DockStyle.Fill;
+                            btnRoom.Margin = new Padding(8);
+                            btnRoom.MinimumSize = new Size(0, 90);
+                            btnRoom.Font = new Font("Microsoft JhengHei", 10, FontStyle.Bold);
+                            btnRoom.Tag = roomNum;
+
+                            switch (status)
+                            {
+                                case "入住中": btnRoom.BackColor = Color.LightCoral; break;
+                                case "待清潔": btnRoom.BackColor = Color.Khaki; break;
+                                case "維修中": btnRoom.BackColor = Color.LightGray; break;
+                                default: btnRoom.BackColor = Color.LightGreen; break;
+                            }
+
+                            btnRoom.Click += RoomButton_Click;
+
+                            int row = roomCount / currentColumnCount;
+                            int col = roomCount % currentColumnCount;
+
+                            if (row >= tlpRooms.RowCount)
+                            {
+                                tlpRooms.RowCount++;
+                                tlpRooms.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+                            }
+
+                            tlpRooms.Controls.Add(btnRoom, col, row);
+                            roomCount++;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region 房間按鈕點擊事件
+        private void RoomButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            selectedRoomNumber = clickedButton.Tag.ToString();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string selectRoomQuery = "SELECT * FROM Rooms WHERE RoomNumber = @RoomNum;";
+
+                using (var command = new SQLiteCommand(selectRoomQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@RoomNum", selectedRoomNumber);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            lblRoomNum.Text = reader["RoomNumber"].ToString();
+                            cmbStatus.SelectedItem = reader["Status"].ToString();
+                            txtNotes.Text = reader["GuestNotes"].ToString();
+
+                            string service = reader["ServiceStatus"].ToString();
+                            cmbService.SelectedItem = string.IsNullOrEmpty(service) ? "無指示" : service;
+
+                            string dateStr = reader["CheckOutDate"].ToString();
+                            if (DateTime.TryParse(dateStr, out DateTime parsedDate))
+                            {
+                                // 🌟 安全防護：如果資料庫裡存的日期比今天還早，就強制顯示今天，避免程式報錯
+                                if (parsedDate < DateTime.Today)
+                                {
+                                    dtpCheckOut.Value = DateTime.Today;
+                                }
+                                else
+                                {
+                                    dtpCheckOut.Value = parsedDate;
+                                }
+                            }
+                            else
+                            {
+                                dtpCheckOut.Value = DateTime.Today;
+                            }
+                            grpEdit.Enabled = true;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region UI 連動：房況狀態下拉選單變更事件
+        private void cmbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbStatus.SelectedItem != null)
+            {
+                string currentStatus = cmbStatus.SelectedItem.ToString();
+
+                if (currentStatus == "空房" || currentStatus == "維修中" || currentStatus == "待清潔")
+                {
+                    dtpCheckOut.Enabled = false;
+                    cmbService.Enabled = false;
+                    cmbService.SelectedItem = "無指示";
+                }
+                else
+                {
+                    dtpCheckOut.Enabled = true;
+                    cmbService.Enabled = true;
+                }
+            }
+        }
+        #endregion
+
+        #region 儲存修改按鈕事件
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedRoomNumber)) return;
+
+            if (cmbStatus.SelectedItem == null)
+            {
+                MessageBox.Show("請選擇房況狀態！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string updateQuery = @"
+                    UPDATE Rooms 
+                    SET Status = @Status, 
+                        CheckOutDate = @CheckOut, 
+                        ServiceStatus = @Service, 
+                        GuestNotes = @Notes 
+                    WHERE RoomNumber = @RoomNum;";
+
+                using (var command = new SQLiteCommand(updateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Status", cmbStatus.SelectedItem.ToString());
+                    command.Parameters.AddWithValue("@CheckOut", dtpCheckOut.Enabled ? dtpCheckOut.Value.ToShortDateString() : "");
+                    command.Parameters.AddWithValue("@Service", cmbService.SelectedItem?.ToString() ?? "無指示");
+                    command.Parameters.AddWithValue("@Notes", txtNotes.Text);
+                    command.Parameters.AddWithValue("@RoomNum", selectedRoomNumber);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show($"房間 {selectedRoomNumber} 資料已成功儲存！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ClearEditArea();
+            LoadRoomButtons();
+        }
+        #endregion
+
+        #region 取消按鈕事件
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearEditArea();
+        }
+        #endregion
+
+        private void ClearEditArea()
+        {
+            selectedRoomNumber = "";
+            lblRoomNum.Text = "-";
+            cmbStatus.SelectedIndex = -1;
+            cmbService.SelectedIndex = -1;
+            dtpCheckOut.Value = DateTime.Now;
+            txtNotes.Clear();
+            grpEdit.Enabled = false;
+        }
+    }
+}
